328b46e5ede12675ddc41e2117d8fda23779d97f
mfreymuth <156535827+mfreymuth@users.noreply.github.com>
2026-03-31 09:48:41 +0200

    feat: add keymap desc for harpoon plugin

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,9 +6,9 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup()
 
-    vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-    vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set('n', '<leader>p', function() harpoon:list():prev() end)
-    vim.keymap.set('n', '<leader>n', function() harpoon:list():next() end)
+    vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Pin buffer in harpoon' })
+    vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Open harpoon pinned buffer list' })
+    vim.keymap.set('n', '<leader>p', function() harpoon:list():prev() end, { desc = 'Go to the previous pinned buffer in the harpoon list' })
+    vim.keymap.set('n', '<leader>n', function() harpoon:list():next() end, { desc = 'Go to the next pinned buffer in the harpoon list' })
   end,
 }
